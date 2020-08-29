884fef97bdcdcc3d424d36a106ad429d11068271
tanvimishra9 <tanvimishra9@gmail.com>
2020-08-29 12:07:46 +0100

    Saturday commit Tanvi

---
+++ src/test/java/Academy/Dummy.java
@@ -0,0 +1,16 @@
+package Academy;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Please study");
+		System.out.println("Please work hard");
+		System.out.println("Please stop using your phone whole day");
+		System.out.println("Please start cleaning dishes");
+		System.out.println("Please chop the vegetables");
+		System.out.println("Please clean the house");
+	}
+
+}
