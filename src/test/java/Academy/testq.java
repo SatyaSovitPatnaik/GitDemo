00e1595b45090a94f60e2b7b375aca8bb0efd034
tanvimishra9 <tanvimishra9@gmail.com>
2020-08-29 15:36:53 +0100

    check1

---
+++ src/test/java/Academy/testq.java
@@ -0,0 +1,14 @@
+package Academy;
+
+public class testq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Caaaaaaa");
+		System.out.println("baaaaaaa");
+		System.out.println("daaaaaaaa");
+
+	}
+
+}
