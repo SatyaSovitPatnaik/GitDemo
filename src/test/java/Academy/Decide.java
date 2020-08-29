5fb0191d2656d2694856ce5257fa675d9f23af72
tanvimishra9 <tanvimishra9@gmail.com>
2020-08-29 13:53:32 +0100

    Added from Master moo

---
+++ src/test/java/Academy/Decide.java
@@ -0,0 +1,13 @@
+package Academy;
+
+public class Decide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Added");
+		System.out.println("Deleted");
+
+	}
+
+}
