3f191ac7f3797494c17f77e07debb2ba5dd75139
tanvimishra9 <tanvimishra9@gmail.com>
2020-08-29 12:11:17 +0100

    Saturday 2nd commit Tanvi

--- src/test/java/Academy/Dummy.java
+++ src/test/java/Academy/Dummy.java
@@ -7,8 +7,8 @@ public class Dummy {
 		
 		System.out.println("Please study");
 		System.out.println("Please work hard");
-		System.out.println("Please stop using your phone whole day");
-		System.out.println("Please start cleaning dishes");
+		
+		
 		System.out.println("Please chop the vegetables");
 		System.out.println("Please clean the house");
 	}
